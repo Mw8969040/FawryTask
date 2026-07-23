b3e4a4c044885a3219019e0f7a6057e19f5ab06d
mohamdWael <mw8969040@gmail.com>
2026-07-23 14:41:46 +0300

    feat : initialize Cars data Class and its properties

--- CarsData.cs
+++ CarsData.cs
@@ -8,5 +8,20 @@ namespace FawryTask
 {
     public class CarsData
     {
+        public string? plateNumber { get; set; }
+        public  DateTime date { get; set; }
+        public Vehicletype vehicleType { get; set; }
+        bool speed { get; set; }
+        bool seatbelt { get; set; }
+
+        public CarsData(string? plateNumber , DateTime date , Vehicletype vehicleType ,bool speed , bool seatbelt)
+        {
+            this.plateNumber = plateNumber;
+            this.date = date;
+            this.vehicleType = vehicleType;
+            this.speed = speed;
+            this.seatbelt = seatbelt;
+        }
+
     }
 }
