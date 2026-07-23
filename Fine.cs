4b7fa2f711d4925e3f7d7ee0326cf19ef3ad1ae9
mohamdWael <mw8969040@gmail.com>
2026-07-23 21:38:24 +0300

    feat : initialize fine Class and write logic

--- Fine.cs
+++ Fine.cs
@@ -8,6 +8,31 @@ namespace FawryTask
 {
     public class Fine
     {
-       
+        public string? plateNumber { get; set; }
+        public List<Violation>? violations { get; set; }
+
+        public Fine(string? plateNumber, List<Violation> violations)
+        {
+            this.plateNumber = plateNumber;
+            this.violations = violations;
+        }
+
+        private decimal TotalAmount => violations?.Sum(v => v.price)??0;
+
+        public void PrintFine()
+        {
+            Console.WriteLine($"Car PlateNumber : {plateNumber}");
+            Console.WriteLine($"Total amount {TotalAmount}");
+            Console.WriteLine($"All Violations");
+
+            foreach (var v in violations)
+            {
+
+                Console.WriteLine($"Description {v.descryption}");
+                Console.WriteLine($"price {v.price}\n");
+
+            }
+            
+        }
     }
 }
--- Rules.cs
+++ Rules.cs
@@ -31,17 +31,54 @@ namespace FawryTask
 
     public class PrivateCarSpeed : Rules
     {
-        public Violation Check(CarsData cars)
+        public Violation? Check(CarsData car)
         {
-            throw new NotImplementedException();
+            if (car == null)
+            {
+                throw new ArgumentNullException("Not found any data for car .");
+            }
+
+            if (car.vehicleType == Vehicletype.PrivateCar && car.speed > 80)
+            {
+                return new Violation(descryption: $"Speed is {car.speed} , exceeded speed limit (80 km/h) .", price: 300);
+            }
+
+            return null;
+        }
+    }
+     public class BusSpeed : Rules
+    {
+        public Violation? Check(CarsData car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentNullException("Not found any data for car .");
+            }
+
+            if (car.vehicleType == Vehicletype.Bus && car.speed > 50)
+            {
+                return new Violation(descryption: $"Speed is {car.speed} , exceeded speed limit (50 km/h) .", price: 300);
+            }
+
+            return null;
         }
     }
 
     public class Seatbelt : Rules
     {
-        public Violation Check(CarsData cars)
+        public Violation? Check(CarsData car)
         {
-            throw new NotImplementedException();
+            if (car == null)
+            {
+                throw new ArgumentNullException("Not found any data for car .");
+            }
+
+            if ((car.vehicleType == Vehicletype.Truck || car.vehicleType == Vehicletype.PrivateCar || car.vehicleType == Vehicletype.Bus)  && car.seatbelt)
+            {
+                return new Violation(descryption: "the seatbelt is not used .", price: 100);
+            }
+
+            return null;
         }
     }
 }
